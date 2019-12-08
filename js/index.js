d71dccdfcccd3363d9cc4dbe6e6fc77cca505503
mtrew2015 <mtrew2015@penguin>
2019-12-08 02:31:42 -0500

    edit some timing

--- js/index.js
+++
@@ -1,8 +0,0 @@
-ScrollReveal().reveal(".web-developer", { delay: 250 });
-ScrollReveal().reveal(".about", { delay: 250 });
-ScrollReveal().reveal(".contactButton", { delay: 1000 });
-ScrollReveal().reveal(".me", { delay: 3000 });
-ScrollReveal().reveal(".project", { delay: 500 });
-ScrollReveal().reveal(".projects", { delay: 500 });
-ScrollReveal().reveal(".contact", { delay: 500 });
-ScrollReveal().reveal(".contact-form", { delay: 500 });
